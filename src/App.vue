327b7d5ca299accb9d895b3f8a9473e779a13699
rehan-sattar <rehansattar117@gmail.com>
2020-07-04 13:13:11 +0500

    📦 NEW: add components for todos and todo items

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todos v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from "./components/Todos";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: "1",
+          name: "go to bank",
+          completed: false
+        },
+        {
+          id: "2",
+          name: "Shopping Center",
+          completed: false
+        },
+        {
+          id: "3",
+          name: "Study Maths",
+          completed: false
+        },
+        {
+          id: "4",
+          name: "Sleep at night!",
+          completed: true
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
 }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="todo__item" v-bind:class="{'is-completed': todo.completed}">
+    <p>
+      <input type="checkbox" v-on:change="markComplete" v-bind:checked="todo.completed" />
+      {{todo.name}}
+    </p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+.todo__item {
+  background-color: #f4f4f4;
+  padding: 10px;
+  border-bottom: 1px dotted #ccc;
+}
+
+.is-completed {
+  text-decoration: line-through;
+}
+</style>
